170ee2c3c07edaf7c612136dca310ba6a60e8eb4
Nashruful <43842557+Nashruful@users.noreply.github.com>
2024-08-07 02:51:49 +0300

    Abdulaziz-Alruwaily

--- bin/assignment8.dart
+++ bin/assignment8.dart
@@ -1,7 +1,7 @@
 import 'model.dart';
 void main() {
 
-  //getAllUsers(); // Print all users in JSON format
+  getAllUsers(); // Print all users in JSON format
    print(getUserByFirstName("Angelia")); // Print object matching first name
    print(getUserByEmail("alexisanthony@rocklogic.com")); // Print object matching email
    print(getChildByFirstName("Hammond")); // Print object's children matching first name
