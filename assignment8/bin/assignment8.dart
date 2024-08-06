7914ca1efe30d31c6c76984d0809d1d1d4fd0158
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-06 18:19:16 +0300

    implements display all users's function

--- assignment8/bin/assignment8.dart
+++ assignment8/bin/assignment8.dart
@@ -1,4 +1,12 @@
-
+import 'data.dart';
+import 'model.dart';
 void main(List<String> arguments) {
    
+   List<Users> myData = [];
+   for (var element in data) {
+    Users user1 = Users.fromJson(element);
+    myData.add(user1);
+     print("\n ${user1.toJson()}");
+     print("______________________________________________");
+   }
 }
--- assignment8/bin/model.dart
+++ assignment8/bin/model.dart
@@ -1,3 +1,25 @@
 class Users{
-  
+  String? id;
+  String? firstName;
+  String? lastName;
+  String? email;
+  Map<String, dynamic> children = {};
+
+  Users({required this.id, required this.firstName, required this.email, required this.lastName, required this.children});
+
+  factory Users.fromJson(Map<String,dynamic> json){
+    return Users(id: json["_id"], firstName: json["first_name"], lastName: json["last_name"], email: json["email"], children: json["children"]);
+  }
+
+
+
+  Map<String, dynamic> toJson(){
+    Map<String, dynamic> map = {};
+    map["_id"] = id;
+    map["first_name"] = firstName;
+    map["last_name"] = lastName;
+    map["email"] = email;
+    map["children"] = children;
+    return map;
+  }
 }
