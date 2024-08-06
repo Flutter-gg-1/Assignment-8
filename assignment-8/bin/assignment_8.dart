f1a97bd7525c814445dd04d1b6a7a1d9bb59cda5
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-06 21:10:23 +0300

    Frist

--- assignment-8/bin/assignment_8.dart
+++ assignment-8/bin/assignment_8.dart
@@ -1,5 +1,4 @@
-import 'package:assignment_8/assignment_8.dart' as assignment_8;
 
-void main(List<String> arguments) {
-  print('Hello world: ${assignment_8.calculate()}!');
+void main() {
+  print('Hello, World!');
 }
