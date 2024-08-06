74adb30f5dc5c57582741b51b589c0ac26d9c607
Basel-75 <basel_1422@outlook.com>
2024-08-06 23:55:47 +0300

    Finish the project

--- bin/handle_data_with_map.dart
+++ bin/handle_data_with_map.dart
@@ -4,5 +4,13 @@ import 'model/user.dart';
 import 'model/user_displayer.dart';
 
 void main(List<String> arguments) {
+  List<User> userinfo = [];
 
+for (var element in Data.userData) {
+  userinfo.add(User.fromJson(element));
+}
+UserDisplayer.printAllUser(userinfo);
+UserDisplayer.printUserByFirstName('Marsha', userinfo);
+UserDisplayer.printUserByEmail('marshagillespie@rocklogic.com', userinfo);
+UserDisplayer.printUserChildren('Marsha', userinfo);
 }
