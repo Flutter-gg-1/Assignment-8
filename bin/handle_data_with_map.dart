a8911b4be2c8bfed52203e2ceb9b7c74634728a3
Basel-75 <basel_1422@outlook.com>
2024-08-06 22:23:17 +0300

    Create : Dart project

---
+++ bin/handle_data_with_map.dart
@@ -0,0 +1,5 @@
+import 'package:handle_data_with_map/handle_data_with_map.dart' as handle_data_with_map;
+
+void main(List<String> arguments) {
+  print('Hello world: ${handle_data_with_map.calculate()}!');
+}
