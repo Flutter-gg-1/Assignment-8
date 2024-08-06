605dc9a1bde8c47f2f76b2979ed8f880a4557997
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-06 17:15:14 +0300

    creat from json for child model

--- model/child.dart
+++ model/child.dart
@@ -7,9 +7,15 @@ class Child {
 
   void parentInfo() {
     print("_" * 20);
-    print("Parent first name: $firstName");
-    print("Parent last name: $lastName");
-    print("Parent email: $email");
-    print("Parent children:");
+    print("child first name: $firstName");
+    print("child last name: $lastName");
+    print("children email: $email");
+  }
+
+  factory Child.fromJson(Map<String, String> json) {
+    return Child(
+        firstName: json['firts_name']!,
+        lastName: json['last_name']!,
+        email: json["email"]!);
   }
 }
