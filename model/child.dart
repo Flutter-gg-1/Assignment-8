d6f79cf063c32a366c1b41e8b0fa619bfe1fac51
Mahdi Al-Helal <mmahdi630@gmail.com>
2024-08-06 17:08:52 +0300

    create child model

--- model/child.dart
+++ model/child.dart
@@ -1,3 +1,15 @@
-class Child{
-  
+class Child {
+  late String firstName;
+  late String lastName;
+  late String email;
+
+  Child({required this.firstName, required this.lastName, required this.email});
+
+  void parentInfo() {
+    print("_" * 20);
+    print("Parent first name: $firstName");
+    print("Parent last name: $lastName");
+    print("Parent email: $email");
+    print("Parent children:");
+  }
 }
